b31f88f0b765226b49cd86b0fefc4ed5dd102569
Naegon <guillaume.hamel@efrei.net>
2020-12-28 12:38:12 +0100

    Some corrections and coments

--- ClientSide/Client.cs
+++ ClientSide/Client.cs
@@ -4,32 +4,36 @@ using Communication;
 
 namespace ClientSide
 {
+    // Each client instance will represent a user connected to a server
     public partial class Client
     {
-        private TcpClient _comm;
-        private UserMsg _currentUser;
-        private bool _messageRunning = false;
+        private UserMsg _currentUser;           // Once logged-in, the current user
+        private bool _messageRunning;   // Used to control while loop in conversations
 
-        public TcpClient Comm { get => _comm; set => _comm = value; }
+        private TcpClient Comm { get; }
 
         public Client(string hostname, int port)
         {
-            _comm = new TcpClient(hostname, port);
+            _messageRunning = false;
+            Comm = new TcpClient(hostname, port);
             Console.WriteLine("--> Connection established on " + hostname + ":" + port);
         }
 
+        // Menu used to either login, register or quit the program
         public void Menu()
         {
+            string choice;
+            
             Console.Clear();
             Console.WriteLine("\nWelcome!");
             Console.WriteLine("1. Login \n2. Register \n3. Quit");
-
-            string choice;
+            
+            // Prompt user for a valid choice
             do
             {
                 Console.Write("\nPlease choose an option: ");
                 choice = Console.ReadLine();
-            } while (!choice.Equals("1") && !choice.Equals("2") && !choice.Equals("3"));
+            } while (choice != null && !choice.Equals("1") && !choice.Equals("2") && !choice.Equals("3"));
 
             switch (choice)
             {
@@ -53,19 +57,24 @@ namespace ClientSide
         }
 
 
+        // Used in separate thread to send message to conversations
         private void SendChat()
         {
             while (_messageRunning)
             {
                 var msg = Console.ReadLine();
-                if (msg.Equals("!quit"))
+                
+                // Check for exit condition
+                if (msg != null && msg.Equals("!quit"))
                 {
-                    _messageRunning = false;
+                    // Break the loop then notify server to stop receiving messages
+                    _messageRunning = false;        
                     Net.SendMsg(Comm.GetStream(), new Request(Net.Action.Quit));
                 }
                 else
                 {
-                    if (!msg.Equals(""))
+                    // Send non-empty messages to the server
+                    if (msg != null && !msg.Equals(""))
                     {
                         Net.SendMsg(Comm.GetStream(), new Chat(_currentUser.Username, msg));
                     }
@@ -73,18 +82,21 @@ namespace ClientSide
                 }
             }
         }
-
+        
+        // Used in separate thread to receive message from conversations
         private void RcvChat()
         {
             while (_messageRunning)
             {
-                Chat chat = (Chat)Net.RcvMsg(Comm.GetStream());
-                if (!(chat.Sender.Equals("") && chat.Content.Equals("")))
-                {
-                    Console.SetCursorPosition(0, Console.CursorTop);
-                    Console.WriteLine(chat);
-                    Console.Write("[" + _currentUser.Username + "] ");
-                }
+                var chat = (Chat)Net.RcvMsg(Comm.GetStream());
+                
+                // Discard empty messages
+                if (chat.Sender.Equals("") && chat.Content.Equals("")) continue;
+
+                // Some formatting of the received message
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.WriteLine(chat);
+                Console.Write("[" + _currentUser.Username + "] ");
             }
         }
     }
